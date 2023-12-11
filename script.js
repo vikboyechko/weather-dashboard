df86ffd61a1a2bc0fae39e31fcb3b3ea868d10b5
Vik Boyechko <vik.boyechko@gmail.com>
2023-12-10 22:15:36 -0600

    app is functional but needs searched-city re-search function

---
+++ script.js
@@ -0,0 +1,158 @@
+
+var searchInput = $('#search-input')
+var searchButton = $('#search-button')
+var resultsEl = $('.results')
+var forecastEl = $('.forecast')
+var resultsCityEl = $('.results-city')
+var resultsIconEl = $('.results-icon')
+var resultsTempEl = $('.results-temp')
+var resultsWindEl = $('.results-wind')
+var resultsHumidityEl = $('.results-humidity')
+
+var WEATHER_API_KEY = 'ef0c4c7ee874fdcca867ed86f58db6b8'
+
+resultsEl.hide();
+forecastEl.hide();
+
+// If local storage exists, get them, and initialize the last search, and populate previous cities searched
+if (!localStorage.getItem('lastSearch')) {
+    localStorage.setItem('lastSearch', '');
+}
+if (!localStorage.getItem('allSearches')) {
+    localStorage.setItem('allSearches', JSON.stringify([]));
+}
+
+// gets the previous searched cities from local storage
+function updateSearchHistory(searchedCity) {
+    localStorage.setItem('lastSearch', searchedCity)
+    var allSearches = JSON.parse(localStorage.getItem('allSearches'));
+    if (!allSearches.includes(searchedCity)) {
+        allSearches.unshift(searchedCity);
+        localStorage.setItem('allSearches', JSON.stringify(allSearches))
+        displayAllSearches();
+    }
+}
+
+// on website load, get the last searched city and call API for the forecast
+var lastSearch = localStorage.getItem("lastSearch")
+getCoords(lastSearch)
+
+// the search button
+searchButton.click(function(event) {
+    event.preventDefault();
+    var searchedCity = searchInput.val();
+    getCoords(searchedCity)
+});
+
+function displayAllSearches() {
+    var allSearches = JSON.parse(localStorage.getItem('allSearches'));
+    var listElement = $('#searched-cities');
+    listElement.empty(); // Clear existing list items
+
+    if (allSearches.length > 0) {
+        listElement.text('Recent searches').css('font-style', 'italic');
+        allSearches.forEach(function(city) {
+            listElement.append('<li>' + city + '</li>');
+        });
+        resultsEl.show();
+        forecastEl.show();
+    } else {
+        listElement.text('');
+        resultsEl.hide();
+        forecastEl.hide();
+    }
+}
+
+displayAllSearches()
+
+// Function to first get latitude and longitude coordinates from zip or city search
+function getCoords(searchInput) {
+
+    var isZipCode = !isNaN(searchInput); // a zip code search gets a different result then city search
+
+    // if search is a zip code. TO DO: limit search to 5 digits
+    if (isZipCode) {
+        var searchApiUrl = `http://api.openweathermap.org/geo/1.0/zip?zip=${searchInput},US&appid=${WEATHER_API_KEY}`
+        fetch(searchApiUrl, {
+            method: 'GET',
+    
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            var resultsCity = data.name;
+            var resultsLat = data.lat;
+            var resultsLon = data.lon;
+            getForecast(resultsLat,resultsLon);
+            
+        })
+    // if search is by city, return first result
+    } else {
+        var searchApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=0&appid=${WEATHER_API_KEY}`
+        fetch(searchApiUrl, {
+            method: 'GET',
+    
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            var resultsCity = data[0].name;
+            var resultsLat = data[0].lat;
+            var resultsLon = data[0].lon;
+            getForecast(resultsLat,resultsLon);
+            
+        })
+    }
+}
+
+// function to get the current weather and 5-day forecast from latitude and longitude
+function getForecast(lat,lon) {
+    var forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${WEATHER_API_KEY}`
+
+    fetch(forecastUrl, {
+        method: 'GET',
+
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        var today = dayjs().format(' (MM/DD/YYYY)')
+        var resultsCity = data.city.name;
+        var resultsTemp = data.list[0].main.temp;
+        var resultsWind = data.list[0].wind.speed;
+        var resultsHumidity = data.list[0].main.humidity;
+        var resultsWeatherIcon = data.list[0].weather[0].icon;
+        var resultsWeatherIconUrl = `http://openweathermap.org/img/wn/${resultsWeatherIcon}.png`
+
+        // Check if local storage exists, and if not, save to local storage, both the last search and the all search keys
+        updateSearchHistory(resultsCity);
+
+        // Populate the current weather
+        resultsCityEl.text(resultsCity).append(today);
+        resultsIconEl.attr('src', resultsWeatherIconUrl);
+        resultsTempEl.text('Temp: ' + Math.trunc(resultsTemp) + '° F') ;
+        resultsWindEl.text('Wind: ' + Math.trunc(resultsWind) + ' mph');
+        resultsHumidityEl.text('Humidity: ' + Math.trunc(resultsHumidity) + ' %')
+        
+        // Loop to populate each card
+        for (var i = 1; i < 6; i++) {
+            var forecastDate = dayjs().add(i, 'day').format('MM/DD/YYYY'); 
+            var forecastTemp = data.list[i].main.temp;
+            var forecastWind = data.list[i].wind.speed;
+            var forecastHumidity = data.list[i].main.humidity;
+            var forecastWeatherIcon = data.list[i].weather[0].icon;
+            var forecastWeatherIconUrl = `http://openweathermap.org/img/wn/${forecastWeatherIcon}.png`
+
+            var dayContainer = $(`.day-container-${i}`);
+            dayContainer.find('.day-date').text(forecastDate);
+            dayContainer.find('.day-icon').attr('src', forecastWeatherIconUrl);
+            dayContainer.find('.day-temp').text('Temp: ' + Math.trunc(forecastTemp) + '° F');
+            dayContainer.find('.day-wind').text('Wind: ' + Math.trunc(forecastWind) + ' mph');
+            dayContainer.find('.day-humidity').text('Humidity: ' + Math.trunc(forecastHumidity) + ' %');
+        }
+    })
+}
+
+
+
+
